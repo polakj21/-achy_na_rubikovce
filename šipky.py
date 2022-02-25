a5344d159de9778eeb6986eb76f92018c8f1f905
Jakub Polák <polakj21@spstrutnov.cz>
2022-02-25 18:28:58 +0100

    všechny možné otočení by měly být možné

--- šipky.py
+++ šipky.py
@@ -471,3 +471,184 @@ class vertical_arrow(pygame.sprite.Sprite):
             self.can_be_used = True
         else:
             self.refresh_countdown -=1
+
+class nevim_arrow(pygame.sprite.Sprite):
+    def __init__(self,pos,lines,top,top_dir):
+        super().__init__()
+        self.image = horizontal["base"]
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.rect.topleft = pos
+        self.lines = lines
+        self.top = top
+        self.top_dir = top_dir
+        self.can_be_used = True
+        self.refresh_countdown = 0
+    def update(self,mouse_pos,colour):
+        if self.can_be_used:
+            if self.rect.collidepoint(mouse_pos):
+                fill(self.lines,self.top,("horizontal","vertical","horizontal","vertical"))
+                if mouse_pos[0] < self.pos[0]+16:
+                    self.image = horizontal["left"]
+                    if pygame.mouse.get_pressed()[0]:
+                        self.move(colour,"left")
+                else:
+                    self.image = horizontal["right"]
+                    if pygame.mouse.get_pressed()[0]:
+                        self.move(colour,"right")
+            else:
+                self.image = horizontal["base"]
+    def move(self,colour,direction):
+        global positions,black,white,kings,boards
+        figures = 0
+        check_kings = 0
+        a = ["","","","","","","",""]
+        b = ["","","","","","","",""]
+        c = ["","","","","","","",""]
+        d = ["","","","","","","",""]
+        A = ["","","","","","","",""]
+        B = ["","","","","","","",""]
+        C = ["","","","","","","",""]
+        D = ["","","","","","","",""]
+        for sym_ind,sym in enumerate(positions[self.lines[0][0]][self.lines[0][1]]):
+            a[sym_ind] = sym
+        for line_ind,line in enumerate(positions[self.lines[1][0]]):
+            for sym_ind,sym in enumerate(line):
+                if sym_ind == self.lines[1][1]:
+                    b[line_ind] = sym
+        for sym_ind,sym in enumerate(positions[self.lines[2][0]][self.lines[2][1]]):
+            c[sym_ind] = sym
+        for line_ind,line in enumerate(positions[self.lines[3][0]]):
+            for sym_ind,sym in enumerate(line):
+                if sym_ind == self.lines[3][1]:
+                    d[line_ind] = sym
+        for sym_ind,sym in enumerate(boards[self.lines[0][0]][self.lines[0][1]]):
+            A[sym_ind] = sym
+        for line_ind,line in enumerate(boards[self.lines[1][0]]):
+            for sym_ind,sym in enumerate(line):
+                if sym_ind == self.lines[1][1]:
+                    B[line_ind] = sym
+        for sym_ind,sym in enumerate(boards[self.lines[2][0]][self.lines[2][1]]):
+            C[sym_ind] = sym
+        for line_ind,line in enumerate(boards[self.lines[3][0]]):
+            for sym_ind,sym in enumerate(line):
+                if sym_ind == self.lines[3][1]:
+                    D[line_ind] = sym
+        lines = (a,b,c,d)
+        for letter in lines:
+            if "k" in letter:
+                return
+            elif "K" in letter:
+                return
+        rects = [pygame.Rect(poss[self.lines[0][0]][0],poss[self.lines[0][0]][1]+3+(self.lines[0][1]*32),256,1),
+                 pygame.Rect(poss[self.lines[1][0]][0]+3+(self.lines[1][1]*32),poss[self.lines[1][0]][1],1,256),
+                 pygame.Rect(poss[self.lines[2][0]][0],poss[self.lines[2][0]][1]+3+(self.lines[2][1]*32),256,1),
+                 pygame.Rect(poss[self.lines[3][0]][0]+3+(self.lines[3][1]*32),poss[self.lines[3][0]][1],1,256)]
+        
+        if colour == "black":
+            for king in kings:
+                if king in black:
+                    check_kings+=1
+            for figure in white:
+                if not figure.checking:
+                    figures+=1
+                else:
+                    for rectangle in rects:
+                        if figure.rect.colliderect(rectangle):
+                            return
+        else:
+            for king in kings:
+                if king in white:
+                    check_kings +=1
+            for figure in black:
+                if not figure.checking and figure not in kings:
+                    figures+=1
+                else:
+                    for rectangle in rects:
+                        if figure.rect.colliderect(rectangle):
+                            return
+        if direction == "left":
+            positions[self.lines[0][0]][self.lines[0][1]] = b
+            for line_ind,line in enumerate(positions[self.lines[1][0]]):
+                line[self.lines[1][1]] = c[-line_ind-1]
+            for sym_ind,sym in enumerate(positions[self.lines[2][0]][self.lines[2][1]]):
+                positions[self.lines[2][0]][self.lines[2][1]][sym_ind] = d[-1-sym_ind]
+            for line_ind,line in enumerate(positions[self.lines[3][0]]):
+                line[self.lines[3][1]] = a[-line_ind-1]
+        else:
+            for sym_ind,sym in enumerate(positions[self.lines[0][0]][self.lines[0][1]]):
+                positions[self.lines[0][0]][self.lines[0][1]][sym_ind] = d[-sym_ind-1]
+                print(positions[self.lines[0][0]][self.lines[0][1]])
+            for line_ind,line in enumerate(positions[self.lines[1][0]]):
+                line[self.lines[1][1]] = a[line_ind]
+            for sym_ind,sym in enumerate(positions[self.lines[2][0]][self.lines[2][1]]):
+                positions[self.lines[2][0]][self.lines[2][1]][sym_ind] = b[-1-sym_ind]
+            for line_ind,line in enumerate(positions[self.lines[3][0]]):
+                line[self.lines[3][1]] = c[line_ind]
+        
+        black.empty()
+        white.empty()
+        kings.empty()
+        set_position()
+        
+        check_kings_2 = 0
+        figures_2 = 0
+        
+        if colour == "black":
+            for king in kings:
+                if king in black:
+                    check_kings_2 +=1
+            for figure in white:
+                if not figure.checking and figure not in kings:
+                    figures_2+=1
+        else:
+            for king in kings:
+                if king in white:
+                    check_kings_2 +=1
+            for figure in black:
+                if not figure.checking and figure not in kings:
+                    figures_2+=1
+                    
+        if figures < figures_2 or check_kings_2 != check_kings:
+            positions[self.lines[0][0]][self.lines[0][1]] = a
+            for line_ind,line in enumerate(positions[self.lines[1][0]]):
+                line[self.lines[1][1]] = b[line_ind]
+            positions[self.lines[2][0]][self.lines[2][1]] = c
+            for line_ind,line in enumerate(positions[self.lines[3][0]]):
+                line[self.lines[3][1]] = d[line_ind]
+            black.empty()
+            white.empty()
+            kings.empty()
+            set_position()
+            return
+        
+        self.image = horizontal["deactive"]
+        self.can_be_used = False
+        self.refresh_countdown =1
+        
+        if direction == "right":
+            for sym_ind,sym in enumerate(boards[self.lines[0][0]][self.lines[0][1]]):
+                boards[self.lines[0][0]][self.lines[0][1]][sym_ind] = D[-sym_ind-1]
+            for line_ind,line in enumerate(boards[self.lines[1][0]]):
+                line[self.lines[1][1]] = A[line_ind]
+            for sym_ind,sym in enumerate(boards[self.lines[2][0]][self.lines[2][1]]):
+                boards[self.lines[2][0]][self.lines[2][1]][sym_ind] = B[-1-sym_ind]
+            for line_ind,line in enumerate(boards[self.lines[3][0]]):
+                line[self.lines[3][1]] = C[line_ind]
+        else:
+            boards[self.lines[0][0]][self.lines[0][1]] = B
+            for line_ind,line in enumerate(boards[self.lines[1][0]]):
+                line[self.lines[1][1]] = C[-line_ind-1]
+            for sym_ind,sym in enumerate(boards[self.lines[2][0]][self.lines[2][1]]):
+                boards[self.lines[2][0]][self.lines[2][1]][sym_ind] = D[-1-sym_ind]
+            for line_ind,line in enumerate(boards[self.lines[3][0]]):
+                line[self.lines[3][1]] = A[-line_ind-1]
+        if self.top != None:
+            rotate(self.top,direction,self.top_dir)
+        
+    def refresh(self):
+        if self.refresh_countdown == 0:
+            self.image = horizontal["base"]
+            self.can_be_used = True
+        else:
+            self.refresh_countdown -=1
